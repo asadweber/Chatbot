60ef18855c50212406ed8604194860076122ec23
Major Asad <m.asaduzzaman@kuwaitarmy.gov.kw>
2026-08-19 17:05:25 +0300

    AA

--- src/Infrastructure/Migrations/20260819140419_InitSql.Designer.cs
+++ src/Infrastructure/Migrations/20260819140419_InitSql.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260819134858_Init")]
-    partial class Init
+    [Migration("20260819140419_InitSql")]
+    partial class InitSql
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
